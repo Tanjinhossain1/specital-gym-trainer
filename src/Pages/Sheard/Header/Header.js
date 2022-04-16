7b9ebfd2f064345bb4849c3b358b453ac8dece25
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-16 22:26:07 +0600

    navbar compleate

---
+++ src/Pages/Sheard/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Gym-Trainer</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">                                                     
+                    </Nav>
+                    <Nav>
+                    <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+                    <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
+                    <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                    <Nav.Link as={Link} to='/signup'>Sign Up</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default header;
