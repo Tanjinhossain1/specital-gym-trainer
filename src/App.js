56c229da87cbff90bfa455454fee9b19c1d0ac7a
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-16 22:36:43 +0600

    banner slider add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './Pages/Sheard/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div>
      <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         
       </Routes>
     </div>
