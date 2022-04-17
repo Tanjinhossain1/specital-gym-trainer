d2b98dbe124528e8b3ea2a714f7b21ef56fd04de
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-17 12:53:50 +0600

    log out implement done

--- src/Pages/Sheard/Header/Header.js
+++ src/Pages/Sheard/Header/Header.js
@@ -1,21 +1,29 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.int';
 
-const header = () => {
+const Header = () => {
+    const [user] = useAuthState(auth);
+    const logout = () => {
+        signOut(auth);
+      };
     return (
         <Navbar sticky='top' collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand as={Link} to="/home">Gym-Trainer</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">                                                     
+                    <Nav className="me-auto">
                     </Nav>
                     <Nav>
-                    <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                    <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
-                    <Nav.Link as={Link} to='/login'>Login</Nav.Link>
-                    <Nav.Link as={Link} to='/signup'>Sign Up</Nav.Link>
+                        <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+                        <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
+                        {user ? <button onClick={()=>logout()} className='border-0 text-white bg-warning rounded-pill ms-4'>Log Out</button> : <Nav.Link as={Link} to='/login'>Login</Nav.Link>}
+
+                    {!user&&    <Nav.Link as={Link} to='/signup'>Sign Up</Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -23,4 +31,4 @@ const header = () => {
     );
 };
 
-export default header;
+export default Header;
